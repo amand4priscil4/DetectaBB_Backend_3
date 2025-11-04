2c0ce4a94eaca55056ecaf19e02fd067092105ce
Amanda Alves <amanda.priscilaa15@gmail.com>
2025-11-04 17:25:31 -0300

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
-# Sair em caso de erro
 set -o errexit
 
-# Atualizar apt-get
+# Atualizar sistema
 apt-get update
 
-# Instalar Tesseract OCR
+# Instalar Tesseract OCR e idioma português
 apt-get install -y tesseract-ocr tesseract-ocr-por
 
 # Instalar dependências Python
